411eba3d08c0c5bf7d8014eed69f121987849468
Joao Silva <joaodoer3@gmail.com>
2024-07-16 18:35:35 -0300

    finalizado crud usando apenas js puro

--- public/index.js
+++ public/index.js
@@ -52,11 +52,12 @@ async function getPosts() {
 }
 async function getPostDetais(id) {
     //Fetch
-    let response = await fetch(`http://localhost:3000/posts/${id}?_embed=comments`);
+    let response = await fetch(`http://localhost:3000/posts/${id}`);
     let data = await response.json();
     //Div principal
     const newDiv = document.createElement('div')
     newDiv.setAttribute('id', 'post')
+    newDiv.setAttribute('class', 'card')
 
 
     //Remocao dos elementos antigos e adicao do novo elemento 
@@ -70,32 +71,33 @@ async function getPostDetais(id) {
     const post = document.getElementById('post')
     post.innerHTML = `<p>${data.title}</p> `
 
-    //Elemento dos comentarios
-    const addComments = document.createElement('div')
-    addComments.setAttribute('id', 'comments')
-    pagina.appendChild(addComments)
 
-    const comments = document.getElementById('comments')
-    comments.innerHTML = data.comments.map((comment) => `<p>${comment.text}</p>`).join('')
-
-    //Elemento form de novo comentario
 
 
     //Icone para voltar ao inicio
     const newGoBack = document.createElement('i')
-    newGoBack.setAttribute('class', 'material-symbols-outlined')
+    newGoBack.setAttribute('class', 'material-symbols-outlined button left')
     newGoBack.setAttribute('id', 'arrow')
     newGoBack.innerHTML = 'arrow_back'
     newGoBack.addEventListener('click', fromGoBack)
     pagina.appendChild(newGoBack)
 
+
+
     //
     const deleteIcon = document.createElement('i')
-    deleteIcon.setAttribute('class', 'material-symbols-outlined')
+    deleteIcon.setAttribute('class', 'material-symbols-outlined button right')
     deleteIcon.setAttribute('id', 'deleteIcon')
     deleteIcon.innerHTML = 'delete'
     deleteIcon.addEventListener('click', function () { deletePost(data) })
     pagina.appendChild(deleteIcon)
+
+    const editIcon = document.createElement('i')
+    editIcon.setAttribute('class', 'material-symbols-outlined button right')
+    editIcon.setAttribute('id', 'editIcon')
+    editIcon.innerHTML = 'edit'
+    editIcon.addEventListener('click', clicked = () => { editPost(data) })
+    pagina.appendChild(editIcon)
 
 
 
@@ -103,20 +105,36 @@ async function getPostDetais(id) {
 
 function fromGoBack() {
     pagina.removeChild(post)
-    pagina.removeChild(comments)
     pagina.removeChild(arrow)
+    pagina.removeChild(editIcon)
     pagina.removeChild(deleteIcon)
     getPosts()
 }
 function deletePost(data) {
-    data.comments.forEach((comment) => { fetch(`http://localhost:3000/comments/${comment.id}`, { method: 'DELETE' }) })
     fetch(`http://localhost:3000/posts/${data.id}`, { method: 'DELETE' })
     pagina.removeChild(post)
-    pagina.removeChild(comments)
     pagina.removeChild(arrow)
+    pagina.removeChild(editIcon)
     pagina.removeChild(deleteIcon)
     getPosts()
 }
+function editPost(data) {
+    editIcon.removeEventListener("click", clicked);
+    post.firstChild.setAttribute('contenteditable', 'true')
+    post.firstChild.setAttribute('style', 'border: 1px solid #ccc')
+    editIcon.innerHTML = 'check'
+    post.firstChild.focus()
+    console.log()
+    editIcon.addEventListener('click', done = () => {
+        fetch(`http://localhost:3000/posts/${data.id}`,
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id: data.id, title: post.firstChild.textContent })
+            }).then(fromGoBack())
+    })
+}
+
 async function newPost(title) {
     if (title) {
         let send = {
@@ -125,6 +143,8 @@ async function newPost(title) {
         const response = await fetch('http://localhost:3000/posts', { method: 'POST', body: JSON.stringify(send), headers: { "Content-type": "application/json; charset=UTF-8" } })
         const data = await response.json()
         getPostDetais(data.id)
+
+
     }
 }
 getPosts()
